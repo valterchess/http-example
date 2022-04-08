0582de1f4724049639f26c08629fc711c1322366
valterchess <valterrekabano7@gmail.com>
2022-04-08 09:19:09 -0300

    trabalhando com java HTTP

---
+++ src/com/httpexample/postRequest.java
@@ -0,0 +1,31 @@
+package com.httpexample;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.time.Duration;
+
+public class postRequest {
+    public static final String URL_POST = "http://httpbin.org/forms/post";
+    public static final String FILE_JSON = "";
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // ciente HTTP
+        HttpClient client = HttpClient.newHttpClient();
+
+        // cria requisição
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofFile(Path.of(FILE_JSON)))
+                .timeout(Duration.ofSeconds(10))
+                .uri(URI.create(URL_POST))
+                .build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+
+    }
+}
